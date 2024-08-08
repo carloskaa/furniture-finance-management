084222e6925c688b1ec7c93cdc6914917625efac
Carlos Caro <ccarom@unal.edu.co>
2024-08-07 23:47:22 -0500

    9

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html")),
+    path("", TemplateView.as_view(template_name="base2.html")),
     path('admin/', admin.site.urls),
     path('', include('finance_control.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
